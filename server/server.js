9b45ef96f5a9b236da66cb7350a1d4345c3b4c72
Ishaan Tyagi <ishaantyagi01@gmail.com>
2025-05-17 17:20:40 +0530

    Fix: Update build configuration and static file serving for Render deployment

--- server/server.js
+++ server/server.js
@@ -16,9 +16,11 @@ app.use('/api/movies', require('./routes/movies'));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  // Set static folder
+  app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
 
